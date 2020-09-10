150bca98b9929080e38da36a5e5075f627fd5021
Aman Kumar <mraman1997@gmail.com>
2020-09-11 01:17:57 +0530

    deploy changes

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ app.get("/data", (req, res) => {
   res.send({ data: "Hello world" });
 });
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, 'client/build')));
     
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
-}
+//   app.get('*', function(req, res) {
+//     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+//   });
+// }
 
 app.listen(5000, () => console.log("Started Listening"));
